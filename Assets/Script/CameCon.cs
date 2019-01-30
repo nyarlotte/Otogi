78e56e06ad838f27362dadabd4005c0964b9286b
Si <bokega>
2019-01-30 23:22:41 +0900

    UnityProjectOtogi

---
+++ Assets/Script/CameCon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameCon : MonoBehaviour
+{
+  /*
+  スクリプトにターゲット欄を追加
+  ターゲットにオブジェクトを追加
+  */
+  public GameObject target =null;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+      /*
+      ターゲットからポジションを取得
+      ターゲットからY軸に1　Z軸に2mのところにカメラを設置する
+      */
+      Vector3 position = target.transform.position;
+      position.y += 1f;
+      position.z -= 2f;
+      transform.position = position;
+    }
+}
---
+++ Assets/Script/Move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKey(KeyCode.W)){
+        transform.Translate(transform.forward*0.1f);
+        //Wを押すと前に進みます 0.1fの数字を大きくすれば前に進むスピードが上がります。
+      }
+      if (Input.GetKey(KeyCode.S)){
+        transform.Translate(transform.forward*-0.1f);
+        //Sを押すと後ろに進みます 0.1fの数字を大きくすれば後ろに進むスピードが上がります。
+      }
+      if (Input.GetKey(KeyCode.D)){
+        transform.Translate(transform.right*0.1f);
+        //Dを押すと右に進みます 0.1fの数字を大きくすれば右に進むスピードが上がります。
+      }
+      if (Input.GetKey(KeyCode.A)){
+        transform.Translate(transform.right*-0.1f);
+        //Aを押すと左に進みます 0.1fの数字を大きくすれば左に進むスピードが上がります。
+      }
+    }
+}
